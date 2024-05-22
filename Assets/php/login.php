688c7bed29e0701d9c6e066603309117715a4078
21020013 <al21020013@itsa.edu.mx>
2024-05-22 11:16:18 -0600

    login

--- Assets/php/login.php
+++ Assets/php/login.php
@@ -1,27 +1,24 @@
 <?php
     if(isset($_POST['submit'])){
         try{
-            $User2 = $_POST['User'];
-            $conexion = new PDO('mysql:host=localhost:3307; dbname=nutriologia', 'root', 'root');
-            
-            $enunciado = $conexion->prepare("SELECT * FROM login");
-            $enunciado->setFetchMode(PDO::FETCH_ASSOC); //devuelve un arreglo asociativo
-            $enunciado->execute();
             $User = $_POST['User'];
             $Password =  $_POST['Password'];
 
-            foreach($enunciado as $row){
-                
-                $usuario = $row['Usuario'];
-                echo $usuario;
-                $contraseña = $row['Contraseña'];
-                echo $contraseña;                
-                    
-                if($User == $usuario && $Password == $contraseña){
+            $conexion = new PDO('mysql:host=localhost:3307; dbname=nutriologia', 'root', 'root');
+            $enunciado = $conexion->prepare("SELECT * FROM login WHERE Usuario = ? ");
+            $enunciado->bindParam(1,$User);
+            $enunciado->execute();
+            
+
+            if($row = $enunciado->fetch()){
+                if($row['Contraseña'] == $Password and $row['Usuario'] == $User){
                     header('Location: http://localhost/SitioWebUber/dashboard.html');
                 }else{
                     header('Location: http://localhost/SitioWebUber/index.html');
                 }
+
+            }else{
+                header('Location: http://localhost/SitioWebUber/index.html');
             }
         }catch(PDOException $e){
             echo "error: ".$e->getMessage();
