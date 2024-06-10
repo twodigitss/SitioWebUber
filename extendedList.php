a8b28d749bdbe0174f400c21cc6932f66200a749
twodigitss <torrescervantesle@gmail.com>
2024-06-10 17:36:32 -0600

    minimas correciones

--- extendedList.php
+++ extendedList.php
@@ -10,56 +10,56 @@
     <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="Assets/css/menu.css"> <!-- Enlazar el CSS -->
     <script src="menu.js"></script> <!-- Enlazar el JavaScript -->
-    <style>
+    <style> <!-- wtf por que? -->
         .panel{
-    font-family: "Fira Sans", sans-serif;
-    font-size: 2rem; font-weight: 500;
-    background-color: var(--col_main);
-    height: 3.5rem;
-    
-    display: flex; 
-    align-items: center;
-    padding: 0px 10px 0px 15px ;
-    margin-bottom: 10px;
-    border: 0px solid #010101;
-    border-radius: 0px;
-
-    .parte2{
-        width: 15%;
-        background-color: none;
-    }
-    .textletras{
-        width: 85%;
-        align-items: center;
-        justify-content: center;
-        .title{
-            padding-left: 10px;
-            color: var(--text);
-            text-align: center; 
+            font-family: "Fira Sans", sans-serif;
+            font-size: 2rem; font-weight: 500;
+            background-color: var(--col_main);
+            height: 3.5rem;
+            
+            display: flex; 
             align-items: center;
+            padding: 0px 10px 0px 15px ;
+            margin-bottom: 10px;
+            border: 0px solid #010101;
+            border-radius: 0px;
+
+            .parte2{
+                width: 15%;
+                background-color: none;
+            }
+            .textletras{
+                width: 85%;
+                align-items: center;
+                justify-content: center;
+                .title{
+                    padding-left: 10px;
+                    color: var(--text);
+                    text-align: center; 
+                    align-items: center;
+                }
+            }
         }
-    }
-}
-@media (min-width:768px) {
-    .panel{
-        .parte2{
-            width: 10%;
+        @media (min-width:768px) {
+            .panel{
+                .parte2{
+                    width: 10%;
+                }
+                .textletras{
+                    width: 90%;
+                }
+            }
         }
-        .textletras{
-            width: 90%;
+        @media (min-width:1200px) {
+            .panel{
+                .parte2{
+                    width: 4%;
+                }
+                .textletras{
+                    width: 90%;
+                }
+            }
         }
-    }
-}
-@media (min-width:1200px) {
-    .panel{
-        .parte2{
-            width: 4%;
-        }
-        .textletras{
-            width: 90%;
-        }
-    }
-}
     </style>
 </head>
 <body>
