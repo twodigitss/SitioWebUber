cf1d32dd47e46de6ae9c925ff03419d52af7586a
21020013 <al21020013@itsa.edu.mx>
2024-06-11 18:19:16 -0600

    fformualrio 2 listo

--- formulario2.php
+++ formulario2.php
@@ -1,49 +1,45 @@
 <?php
-$nombre = isset($_GET['nombres']) ? $_GET['nombres'] : '';
+// Obtener el nombre de forma segura desde la URL
+$nombre = isset($_GET['nombres']) ? htmlspecialchars($_GET['nombres']) : '';
+
+// Construir la extensión de la URL para mantener el nombre en los redireccionamientos
 $urlExtension = "?nombres=" . urlencode($nombre);
 
-if (isset($_POST['registro'])) { // el nombre del boton (submit) es registro 
-    $nombre2 = $_POST['yamequieroir'];
-    $ante = $_POST['ant'];
-    $activ = $_POST['act'];
-    $hor = $_POST['horarios'];
-    $carb = $_POST['carbohidratos'];
-    $agua = $_POST['agua'];
-    $cint = $_POST['cintura'];
-    $cad = $_POST['cadera']; 
-    $braz = $_POST['brazo']; 
-    $alt = $_POST['altura']; 
-    $peso = $_POST['peso']; 
-    $imc = $_POST['imc']; 
-    $grasa = $_POST['grasa']; 
-    $bpm = $_POST['bpm']; 
-    $otros = $_POST['otros']; 
+// Procesar el formulario cuando se envíe
+if (isset($_POST['registro'])) { 
+    // Recopilar datos del formulario de manera segura
+    $nombre2 = htmlspecialchars($_POST['yamequieroir']);
+    $ante = htmlspecialchars($_POST['ant']);
+    $activ = htmlspecialchars($_POST['act']);
+    $hor = htmlspecialchars($_POST['horarios']);
+    $carb = htmlspecialchars($_POST['carbohidratos']);
+    $agua = htmlspecialchars($_POST['agua']);
+    $cint = htmlspecialchars($_POST['cintura']);
+    $cad = htmlspecialchars($_POST['cadera']); 
+    $braz = htmlspecialchars($_POST['brazo']); 
+    $alt = htmlspecialchars($_POST['altura']); 
+    $peso = htmlspecialchars($_POST['peso']); 
+    $imc = htmlspecialchars($_POST['imc']); 
+    $grasa = htmlspecialchars($_POST['grasa']); 
+    $bpm = htmlspecialchars($_POST['bpm']); 
+    $otros = htmlspecialchars($_POST['otros']); 
 
+    // Establecer conexión a la base de datos
     $conexion = new PDO('mysql:host=localhost:3307;dbname=nutriologia', 'root', 'root');
+    
+    // Preparar la consulta SQL para inserción
     $enunciado = $conexion->prepare("INSERT INTO datos_especificos VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
     
-    $enunciado->bindParam(1, $ante);
-    $enunciado->bindParam(2, $activ);
-    $enunciado->bindParam(3, $hor);
-    $enunciado->bindParam(4, $carb);
-    $enunciado->bindParam(5, $agua);
-    $enunciado->bindParam(6, $cint);
-    $enunciado->bindParam(7, $cad);
-    $enunciado->bindParam(8, $braz);
-    $enunciado->bindParam(9, $alt);
-    $enunciado->bindParam(10, $peso);
-    $enunciado->bindParam(11, $imc);
-    $enunciado->bindParam(12, $grasa);
-    $enunciado->bindParam(13, $bpm);
-    $enunciado->bindParam(14, $otros);
-    $enunciado->bindParam(15, $nombre2);
+    // Vincular parámetros y ejecutar la consulta
+    $enunciado->execute([$ante, $activ, $hor, $carb, $agua, $cint, $cad, $braz, $alt, $peso, $imc, $grasa, $bpm, $otros, $nombre2]);
     
-    $enunciado->execute();
+    // Redirigir al usuario después de procesar el formulario
     header("Location: http://localhost/SitioWebUber/perfilcliente.php" . $urlExtension);
     exit();
 }
 
-if (isset($_POST['cancel'])) { // el nombre del boton (submit) es cancel 
+// Redirigir si se cancela el formulario
+if (isset($_POST['cancel'])) { 
     header("Location: http://localhost/SitioWebUber/perfilcliente.php" . $urlExtension);
     exit();
 }
@@ -58,57 +54,58 @@ if (isset($_POST['cancel'])) { // el nombre del boton (submit) es cancel
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@100..900&family=Nunito:wght@200..1000&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="Assets/css/formulario2.css">
-    <link rel="stylesheet" type="text/css" href="Assets/css/menu.css"> <!-- Enlazar el CSS -->
-    <script src="menu.js"></script> <!-- Enlazar el JavaScript -->
-    <title>Info. Paciente</title>
+    <link rel="stylesheet" type="text/css" href="Assets/css/menu.css"> 
+    <script src="menu.js"></script> 
+    <title>Información del Paciente</title>
 </head>
 <body>
     <header class="panel"> 
-        <div class="parte2" id="menu-container"></div> <!-- El menú se cargará aquí -->
+        <div class="parte2" id="menu-container"></div> 
         <div class="textletras"> <p class="title">Healthsync</p> </div>     
     </header>
         
     <main class="contenedor">      
         <section>
-            <h2>Datos especificos</h2>
-            <form action="<?php echo $_SERVER['PHP_SELF'] . $urlExtension; ?>" name="formu2" method="post" class="formulario2"> 
-                <input class="input-text" type="hidden" name="yamequieroir" value="<?php echo htmlspecialchars($nombre); ?>">      
+            <h2>Datos Específicos</h2>
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'] . $urlExtension); ?>" name="formu2" method="post" class="formulario2"> 
+                <input class="input-text" type="hidden" name="yamequieroir" value="<?php echo $nombre; ?>">      
                 <div class="contenedor-campos contenedor">
-                    
                     <div class="primera-division">
                         <div class="campo">
-                            <label>Antecedentes clínicos: </label>
-                            <br>
-                            <input class="input-text" type="text" name="ant">
-                            <br>
-                            <label>Actividad física: </label>
-                            <br>
-                            <input class="input-text" type="text" name="act">
-                            <br>
-                            <label>Horarios de comida: </label>
-                            <br>
-                            <input class="input-text" type="float" name="horarios">
-                            <br> 
-                            <label>Cantidad de carbohidratos</label>
-                            <br>
-                            <input class="input-text" type="text" name="carbohidratos">
-                            <br>
-                            <label>Cantidad de agua</label>
-                            <br>
-                            <input class="input-text" type="text" name="agua">
-                            <br>
-                            <label>Cintura</label>
-                            <br>
-                            <input class="input-text" type="text" name="cintura">
-                            <br>
-                            <label>Cadera</label>
-                            <br>
-                            <input class="input-text" type="text" name="cadera">
+                            <div class="campo2">
+                                <label>Antecedentes clínicos: </label>
+                                <br>
+                                <input class="input-text" type="text" name="ant">
+                                <br>
+                                <label>Actividad física: </label>
+                                <br>
+                                <input class="input-text" type="text" name="act">
+                                <br>
+                                <label>Horarios de comida: </label>
+                                <br>
+                                <input class="input-text" type="float" name="horarios">
+                                <br> 
+                                <label>Cantidad de carbohidratos</label>
+                                <br>
+                                <input class="input-text" type="text" name="carbohidratos">
+                                <br>
+                                <label>Cantidad de agua</label>
+                                <br>
+                                <input class="input-text" type="text" name="agua">
+                                <br>
+                                <label>Cintura</label>
+                                <br>
+                                <input class="input-text" type="text" name="cintura">
+                                <br>
+                                <label>Cadera</label>
+                                <br>
+                                <input class="input-text" type="text" name="cadera">
+                            </div>
                         </div>
                     </div>
-
                     <div class="segunda-division">
                         <div class="campo">   
+                            <div class="campo2">
                             <label>Brazo</label>
                             <br>
                             <input class="input-text" type="text" name="brazo">
@@ -136,12 +133,13 @@ if (isset($_POST['cancel'])) { // el nombre del boton (submit) es cancel
                             <label>Otros factores </label>
                             <br>
                             <input class="input-text" type="text" name="otros">
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div class="botn">
-                <input class='botn1' type="submit" name="registro" value="enviar">
-                <input class='botn1' type="submit" name="cancel" value="cancelar">
+                    <input class='botn1' type="submit" name="registro" value="Enviar">
+                    <input class='botn1' type="submit" name="cancel" value="Cancelar">
                 </div>
             </form>
         </section>
